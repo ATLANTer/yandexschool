f671d554518960fc01a46dd3123c8ec6c7125aba
ATLANTer <72944305+ATLANTer@users.noreply.github.com>
2021-06-11 21:47:53 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -3,15 +3,15 @@ import os, json, requests, argparse
 URL = "https://api.github.com/search/repositories"
 
 
-def argument_parsing():
-    global args
+def argument_parsing(): # python parser.py --help
     arguments = argparse.ArgumentParser()
     arguments.add_argument('-v', '--value', type=str, help='строка для поиска')
     arguments.add_argument('-f', '--file', type=str, help='путь к выходному json файлу')
     args = arguments.parse_args()
+    return args
 
 
-def validation_args():
+def validation_args(args):
     if args.value is None or args.file is None:
         print('Check your starting arguments. That is incorrect.')
         exit()
@@ -22,26 +22,24 @@ def validation_args():
             exit()
 
 
-def get_query():
-    items_dict = dict()
-    items_dict['items'] = list()
+def get_query(args):
+    items_list = list()
     keyword_repositories = requests.get(URL, params={'q': args.value, 'sort': 'stars', 'order': 'desc', 'per_page': 100, 'page': 1})
-    items_dict['items'].extend(keyword_repositories.json().get('items'))
     counter = 2
-    while keyword_repositories.status_code == 200 and keyword_repositories.json()['items']:
+    while keyword_repositories.status_code == 200 and keyword_repositories.json().get('items') is not None:
+        items_list.extend(keyword_repositories.json().get('items'))
         keyword_repositories = requests.get(URL, params={'q': args.value, 'sort': 'stars', 'order': 'desc', 'per_page': 100, 'page': counter})
-        items_dict['items'].extend(keyword_repositories.json()['items'])
         counter += 1
-    return items_dict
+    return items_list
 
 
-def write_file(data):
-    with open(args.file, "w") as file:
+def write_file(data, saved_file):
+    with open(saved_file, "w") as file:
         json.dump(data, file, indent=2)
 
 
 if __name__ == "__main__":
-    argument_parsing()
-    validation_args()
-    data = get_query()
-    write_file(data)
+    arguments = argument_parsing()
+    validation_args(arguments)
+    data = get_query(arguments)
+    write_file(data, arguments.file)
